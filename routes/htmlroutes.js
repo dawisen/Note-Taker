59923f0c494be20df527768ba769ffa8669091de
unknown <danielle>
2020-11-20 21:30:31 -0500

    changed files

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -16,14 +16,14 @@ module.exports = function (app) {
   // ---------------------------------------------------------------------------
 
   app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "../../public/notes.html"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 
   // If no matching route is found default to home
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../../public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../../public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-var PORT = process.env.HOST || 8080;
+var PORT = process.env.PORT || 8080;
 //these app.use handlers parse the note data
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({
@@ -10,8 +10,8 @@ app.use(express.urlencoded({
 }))
 //require these route files so that the server's get request returns
 //and displays the html and api pages
-require('./js/routes/apiRoutes')(app);
-require('./js/routes/htmlroutes')(app);
+require('./routes/apiRoutes')(app);
+require('./routes/htmlroutes')(app);
 
 
 app.listen(PORT, function () {
